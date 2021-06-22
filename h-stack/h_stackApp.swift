57219e1d7ea9538d71a4c8b2276719110cc2b434
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-22 11:25:53 -0700

    Initial Commit

---
+++ h-stack/h_stackApp.swift
@@ -0,0 +1,17 @@
+//
+//  h_stackApp.swift
+//  h-stack
+//
+//  Created by user on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct h_stackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
